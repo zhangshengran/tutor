9ca75619cdc55a96ffea13146ba158359506d4ab
zhangshengran <1286693585@qq.com>
2018-12-07 11:05:08 +0800

    解决输入账号不存在报错问题

--- back/expressAPI/models/login.js
+++ back/expressAPI/models/login.js
@@ -1,48 +1,55 @@
 const mysql = require('mysql'),
-con = mysql.createConnection({
-    host: '39.105.111.123',
-    user: 'root',
-    password: 'xingyun2016',
-    database: 'tutor'
-});
-      
-        exports.select_stu= function(req,res) {
-           var stu_phone = req.body.stu_phone;
-           var stu_password =  req.body.stu_password;
-            con.query('select stu_password,stu_id from students where stu_phone = ?', [stu_phone], (err, result) => {
-            
-                if (err) {
-                    res.send( {
-                        status:1,
-                        info    : 'error',
-                        message:'数据库连接失败'
-                    })
-                }else
-                {    
-                console.log('数据库查到的ID' + result[0].stu_id);              
+    con = mysql.createConnection({
+        host: '39.105.111.123',
+        user: 'root',
+        password: 'xingyun2016',
+        database: 'tutor'
+    });
+
+exports.select_stu = function (req, res) {
+    var stu_phone = req.body.stu_phone;
+    var stu_password = req.body.stu_password;
+
+    con.query('select stu_password,stu_id from students where stu_phone = ?', [stu_phone], (err, result) => {
+
+        if (err) {
+            res.send({
+                status: 1,
+                info: 'error',
+                message: '数据库连接失败'
+            })
+        } else {
+            if (result.length == 0) {
+                res.send({
+                    status: 1,
+                    info: 'error',
+                    message: '此账号不存在'
+                })
+            }
+            else {
+                console.log('数据库查到的ID' + result[0].stu_id);
                 console.log('数据库查到的密码' + result[0].stu_password);
-                if(result[0].stu_password ==stu_password)
-                {
-                    
-                    res.send( {
-                        status:0,
-                        info    : 'OK',
-                        tokenID:result[0].stu_id,
-                        message:'密码匹配正确'
+                if (result[0].stu_password == stu_password) {
+
+                    res.send({
+                        status: 0,
+                        info: 'OK',
+                        tokenID: result[0].stu_id,
+                        message: '密码匹配正确'
                     })
                 }
-                else
-                {
-                    res.send( {
-                        status:1,
-                        info    : 'error',
-                        message:'密码匹配错误'
+                else {
+                    res.send({
+                        status: 1,
+                        info: 'error',
+                        message: '密码匹配错误'
                     })
                 }
-               
-                
-                }
-                
-            });
-        } 
-        
+
+
+            }
+
+        }
+
+    });
+}
