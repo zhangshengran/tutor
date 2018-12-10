b57e90999d2641564da40f7d9e4befdffb2937c8
zhangshengran <1286693585@qq.com>
2018-12-10 19:21:21 +0800

    1

--- back/expressAPI/models/order.js
+++ back/expressAPI/models/order.js
@@ -6,10 +6,10 @@ exports.order_set = function (req, res) {
     var order_time = req.body.order_time;
     var class_time = req.body. class_time;
     var order_address = req.body.order_address;
+    var order_course = req.body.order_course;
 
-
-    con.query('insert into order2(stu_id,tea_id,order_address,order_time,class_time)  values(?,?,?,?,?)',
-        [stu_id,tea_id, order_address, order_time,class_time], (err, result) => {
+    con.query('insert into order2(stu_id,tea_id,order_address,order_time,class_time,order_course)  values(?,?,?,?,?,?)',
+        [stu_id,tea_id, order_address, order_time,class_time,order_course], (err, result) => {
             if (err) {
               
                 res.send({
@@ -33,10 +33,8 @@ exports.order_set = function (req, res) {
 // 学生要查询的订单
 exports.select_order_stu = function(req,res){
     var stu_id = req.query.stu_id;
-     var select2 = 'select tea_name,tea_sex,stu_grade,stu_courses,tea_school,tea_major,tea_grade,'
-                 +'order_id,class_time,order_time,order_address '
-                 +'from teachers,order2 where teachers.tea_id = order2.tea_id and stu_id =? '
-    con.query(select2,[stu_id], (err, result) => {
+    console.log(stu_id);
+    con.query('select tea_name,tea_sex,stu_grade,stu_courses,tea_school,tea_major,tea_grade, order_id,class_time,order_time,order_address,order_course from teachers,order2 where teachers.tea_id = order2.tea_id and stu_id =?',[stu_id], (err, result) => {
         if(err){
            
             res.send({
@@ -56,7 +54,7 @@ exports.select_order_stu = function(req,res){
 exports.select_order_tea = function(req,res){
     var tea_id = req.query.tea_id;
     var select3 = 'select stu_name,stu_age,stu_sex,stu_grade,stu_phone,'
-                +'order_id,class_time,order_time,order_address '
+                +'order_id,class_time,order_time,order_address,order_course '
                 +'from students,order2 where students.stu_id = order2.stu_id and tea_id =? '
     con.query(select3,[tea_id], (err, result) => {
         if(err){
