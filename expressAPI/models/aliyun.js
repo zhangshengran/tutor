d44b89940f31ae7605302253128442a2e2be08c9
zhangshengran <1286693585@qq.com>
2020-03-03 10:58:08 +0800

    123
    123

--- expressAPI/models/aliyun.js
+++ expressAPI/models/aliyun.js
@@ -4,9 +4,9 @@ var con = require('./db').con;
 
 let client = new OSS({
   region: 'oss-cn-beijing',
-  accessKeyId: 'LTAILzRjytI5AIO8',
-  accessKeySecret: 'VtwJkolPmWi0k3RvxKqtG5v1wMSBWm', 
-  bucket: 'tutor666',
+  accessKeyId: '',
+  accessKeySecret: '', 
+  bucket: '',
 });
 
 // path为指定阿里云OSS的上传位置（记得加 ‘/’，例如 head/，filename为上传的文件命名，temfile为本地服务器存储位置）
